4009ae439e05a3cb83745b14ad72721372060def
Richie Yoro <7backspace4@gmail.com>
2023-08-04 16:13:33 -0700

    Simplified the routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -22,22 +22,16 @@ $routes->set404Override();
  */
 $routes->get('api/getUserSeminars/(:num)', 'Api::getUserSeminars/$1');
 
+//********************
 
+$routes->get('home', 'AccountController::index');
+$routes->match(['get', 'post'], 'loginAuth', 'AccountController::loginAuth');
+$routes->match(['get', 'post'], 'dashboard', 'AccountController::login', ['filter' => 'authGuard']);
+$routes->get('addAccount', 'AccountController::addAccount');
+$routes->match(['get', 'post'], 'addAccount', 'AccountController::storeAccount');
 
-/* Default Route */
-$routes->get('home', 'SigninController::index');
+//********************
 
-/* Signup Controller Routes */
-$routes->get('/new_register', 'SignupController::index');
-$routes->match(['get', 'post'], 'new_register', 'SignupController::store');
-
-/* Signin Controller Routes */
-$routes->match(['get', 'post'], 'SigninController/loginAuth', 'SigninController::loginAuth');
-
-/* Profile Controller Routes */
-//$routes->get('/adminprofile', 'ProfileController::admin',['filter' => 'authGuard']);
-//$routes->get('/ownerprofile', 'ProfileController::owner',['filter' => 'authGuard']);
-$routes->get('/dashboard', 'ProfileController::login', ['filter' => 'authGuard']);
 
 /* Seminar Controller Routes */
 $routes->match(['get', 'post'], 'SeminarController/create', 'SeminarController::create');
