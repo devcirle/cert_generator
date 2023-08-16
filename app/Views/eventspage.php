36b8d0ae5fa05f9a14a32117a47ebfadfb4475a1
Richie Yoro <7backspace4@gmail.com>
2023-08-16 11:58:30 -0700

    headerstyle css

--- app/Views/eventspage.php
+++ app/Views/eventspage.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:wght@400;700&display=swap" rel="stylesheet">
 
-    <link rel=stylesheet href="css/eventspage.css">
-    <link rel=stylesheet href="css/header.css">
+    <!-- <link rel=stylesheet href="css/eventspage.css"> -->
+    <link rel=stylesheet href="css/headerstyle.css">
+    <link rel=stylesheet href="css/global.css">
 
     <title>Document</title>
 </head>
